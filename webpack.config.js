11771efa14385174c107e0db180e366ac2bfd57b
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-04-29 08:50:34 +0200

    Issues fixed with devServer and cors

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,15 @@ module.exports = {
 		sourceMapFilename: 'paella-player.js.map'
 	},
 	devtool: 'source-map',
+	devServer: {
+		port: 8080,
+		disableHostCheck: true,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+			"Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+		}
+	},
 	
 	module: {
 		rules: [
