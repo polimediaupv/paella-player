c09e711be7e8d113561b3de7f55ff8c85a5efb7c
Miguel Angel Valero Navarro <miguel.angel.valero.navarro@cern.ch>
2021-11-11 18:29:59 +0100

    Nested events from repo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 
+
+import { utils } from 'paella-core';
 import { Paella } from 'paella-core';
 import getBasicPluginContext from 'paella-basic-plugins';
 import getSlidePluginContext from 'paella-slide-plugins';
 import getfreakyloopPluginContext from 'paella-freakyloop-plugin';
 import getUserTrackingPluginContext from 'paella-user-tracking';
+
+export async function getMyVideoIdFunction() {
+    console.debug("Using MY specific getVideoId function");
+    if (utils.getUrlParameter("year")){
+        return (utils.getUrlParameter("year") + "/" + utils.getUrlParameter("id"));
+    }else{
+        return utils.getUrlParameter("id") 
+    }
+}
+
+export async function getMyVideoYearFunction() {
+    console.debug("Using MY specific getMyVideoYear function");
+    return utils.getUrlParameter("year");
+}
+
+export async function getMyRepositoryURLFunction() {
+    console.debug("Using MY specific getMyRepositoryURL function");
+    return 'media_data';
+}
+
+export async function getMyManifestUrlFunction() {
+    console.debug("Using MY specific getManifestUrl function");
+    return utils.joinPath([getMyRepositoryURLFunction(), getMyVideoYearFunction(), getMyVideoIdFunction()]);
+}
+
+export async function getMySessionFunction() {
+    console.debug("Using MY specific getMySession function");
+    console.debug(utils.getUrlParameter("session"));
+    return utils.getUrlParameter("session");
+}
+
 
 const initParams = {
     customPluginContext: [
@@ -12,7 +45,9 @@ const initParams = {
         getfreakyloopPluginContext(),
         getSlidePluginContext(),
         getUserTrackingPluginContext()
-    ]
+    ],
+    repositoryUrl: 'repository',
+    getVideoId: getMyVideoIdFunction
 };
 
 let paella = new Paella('player-container', initParams);
