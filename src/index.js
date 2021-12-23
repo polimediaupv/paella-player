36b163c459b53bd7bc52f285ef1e25ba6a435797
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-12-23 11:28:26 +0100

    New version attribute added to player instance

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import getSlidePluginContext from 'paella-slide-plugins';
 import getZoomPluginContext from 'paella-zoom-plugin';
 import getUserTrackingPluginContext from 'paella-user-tracking';
 
+import packageData from "../package.json";
+
 const initParams = {
     customPluginContext: [
         require.context("./plugins", true, /\.js/),
@@ -15,7 +17,20 @@ const initParams = {
     ]
 };
 
-let paella = new Paella('player-container', initParams);
+class PaellaPlayer extends Paella {
+    get version() {
+        const player = packageData.version;
+        const coreLibrary = super.version;
+        const pluginModules = this.pluginModules.map(m => `${ m.moduleName }: ${ m.moduleVersion }`);
+        return {
+            player,
+            coreLibrary,
+            pluginModules
+        };
+    }
+}
+
+let paella = new PaellaPlayer('player-container', initParams);
 
 paella.loadManifest()
     .then(() => console.log("done"))
