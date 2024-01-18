3f11f9407f1c8bf7b160b22a739e5eee50f86389
Fernando Serrano Carpena <ferserc1@gmail.com>
2024-01-18 09:01:20 +0100

    initial commit for version 2.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# Paella Player (version 7 and beyond)
+
+This is the main repo of Paella Player for versions 7 and beyond. Note that Paella Player plugins may live in different repos.
+
+Build Paella Player:
+
+```zsh
+npm install
+npm run build
+```
+
+The player will be located in the `dist` directory.
+
+To test Paella Player locally:
+
+```zsh
+npm install
+npm run dev
+```
+
+Then open the following URL in a browser:
+
+[http://localhost:8080/?id=belmar-multiresolution-remote](http://localhost:8080/?id=belmar-multiresolution-remote)
+
+Documentation lives here:
+[https://github.com/polimediaupv/paella-core/blob/main/doc/index.md]
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: 'paella-player.js',
+		sourceMapFilename: 'paella-player.js.map'
+	},
+	devtool: 'source-map',
+	devServer: {
+		port: 8080,
+		allowedHosts: 'all',
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+			"Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization, range"
+		}
+	},
+	
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+
+			{
+				test: /\.js$/,
+				enforce: 'pre',
+				use: ['source-map-loader']
+			},
+
+			{
+				test: /\.css$/,
+				use:  [
+					'style-loader',
+					'css-loader'
+				]
+			},
+
+			{
+				test: /\.svg$/i,
+				use: {
+					loader: 'svg-inline-loader'
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: './config', to: 'config' },
+				{ from: './src/index.html', to: 'index.html' },
+				{ from: './repository_test/repository', to: 'repository' },
+				{ from: './src/style.css', to: 'style.css' }
+			]
+		})
+	],
+
+	performance: {
+		hints: false,
+		maxEntrypointSize: 1048576,
+		maxAssetSize: 1048576
+	}
+}
