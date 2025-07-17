baa48856d865f926e5ba42841a0acb6751992ac3
Miguel Escriva <miesgre@gmail.com>
2025-07-17 13:57:31 +0200

    Changes in typescript types

---
+++ repos/paella-user-tracking/changelog.md
@@ -0,0 +1,4 @@
+# Changelog
+
+- 2.2.3:
+    * Changes in typescript types
--- repos/paella-user-tracking/src/paella-user-tracking.d.ts
+++ repos/paella-user-tracking/src/paella-user-tracking.d.ts
@@ -1,11 +1,39 @@
 declare module "@asicupv/paella-user-tracking" {
-    import type { PluginRef, EventLogPlugin, DataPlugin } from "@asicupv/paella-core";
+    import type { PluginRef, EventLogPlugin, DataPlugin, DataPluginConfig, EventName } from "@asicupv/paella-core";
 
     export const userTrackingPlugins: PluginRef[];
     export const allPlugins: PluginRef[];
 
-    export class GoogleAnalyticsUserTrackingDataPlugin extends DataPlugin {}
-    export class MatomoUserTrackingDataPlugin extends DataPlugin {}
+    export type UserTrackingData = {
+        event: EventName;
+        params: object;
+    }
+
+    export type GoogleAnalyticsUserTrackingDataPluginConfig = DataPluginConfig & {
+        trackingId?: string;
+        domain?: string;
+        category?: string;
+    }
+
+
+    export class GoogleAnalyticsUserTrackingDataPlugin<C extends GoogleAnalyticsUserTrackingDataPluginConfig = GoogleAnalyticsUserTrackingDataPluginConfig> extends DataPlugin<C, UserTrackingData> {}
+
+    export type MatomoUserTrackingDataPluginConfig = DataPluginConfig & {        
+        server?: string;
+        siteId?: string;
+        trackerUrl?: {
+            php?: string;
+            js?: string;
+        };
+        matomoGlobalLoaded?: boolean;
+        mediaAnalyticsTitle?: string;
+        cookieType?: string;        
+        events?: Record<string, string>;
+        customDimensions?: Record<string, string>;
+    }
+    
+    export class MatomoUserTrackingDataPlugin<C extends MatomoUserTrackingDataPluginConfig = MatomoUserTrackingDataPluginConfig> extends DataPlugin<C, UserTrackingData> {}
+
     export class DebugUserTrackingDataPlugin extends DataPlugin {}
     export class UserEventTrackerPlugin extends EventLogPlugin {}
 }
