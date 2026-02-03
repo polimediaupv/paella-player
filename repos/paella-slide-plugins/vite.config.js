d8f747c21b3ac486608036f3d3a3987a4fc011ca
Fernando Serrano Carpena <ferserc1@gmail.com>
2026-02-03 09:19:37 +0100

    Add TypeScript support to slide navigation plugins with updated build configuration

--- repos/paella-slide-plugins/vite.config.js
+++ repos/paella-slide-plugins/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
-import path from 'node:path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     root: './src',
@@ -23,13 +22,11 @@ export default defineConfig({
         sourcemap: true
     },
     plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'paella-slide-plugins.d.ts',
-                    dest: '../dist'
-                }
-            ]
+        dts({
+            entryRoot: './',
+            outDir: '../dist',
+            include: ['**/*.ts'],
+            exclude: ['**/*.test.ts', '**/*.spec.ts']
         })
     ]
 });
