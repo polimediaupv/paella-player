6b1303cc0d30b85e6bcd7dac86b43d9177d3fade
Fernando Serrano Carpena <ferserc1@gmail.com>
2026-07-23 10:18:10 +0200

    Fix minor TypeScript bug in PluginModule

--- repos/paella-core/src/js/core/PluginModule.ts
+++ repos/paella-core/src/js/core/PluginModule.ts
@@ -3,12 +3,12 @@ import { type Dictionaries } from "./Localization";
 
 export default class PluginModule {
     get moduleName() : string {
-        console.warn(`Incomplete player module definition: '${ __filename }.moduleName'`);
+        console.warn(`Incomplete player module definition: '${ this.constructor.name }.moduleName'`);
         return "-";
     }
 
     get moduleVersion() : string {
-        console.warn(`Incomplete player module definition: '${ __filename }.moduleVersion'`);
+        console.warn(`Incomplete player module definition: '${ this.constructor.name }.moduleVersion'`);
         return "0.0.0";
     }
 
